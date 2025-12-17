de685873a406afdceb60aebd64a87dca6571ea53
mayanksharmadesign <mayanksharma.formal@gmail.com>
2025-12-17 22:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import streamlit as st
 import pickle
-
-# ===================== LOAD MODEL =====================
 model = pickle.load(open("model.pkl", "rb"))
-
-# ===================== UI =====================
 st.title("SMS Spam Classifier")
 
 sms = st.text_area("Enter SMS")
@@ -13,12 +9,9 @@ if st.button("Predict"):
     if sms.strip() == "":
         st.warning("Please enter a message")
     else:
-        # IMPORTANT: pass RAW text to the pipeline
         prediction = model.predict([sms])[0]
 
         if prediction == 1:
             st.error("🚨 Spam Message")
         else:
             st.success("✅ Not Spam")
-
-
